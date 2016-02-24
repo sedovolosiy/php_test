53c1570eab84981e033783ac1096a2c9241c7f04
Siedovolosij Serhij <sedovolosiy@gmail.com>
2016-02-24 16:53:05 +0200

    Add form to index.php (testing)

--- index.php
+++ index.php
@@ -6,23 +6,27 @@
 </head>
 <body>
 <form action="?circle=1" method="post">
-    <input type="number" name="radius" step="1" value="" placeholder="Введите радиус круга">
-    <input type="number" name="x" step="1" value="" placeholder="Введите координату 'X' фигуры">
-    <input type="number" name="y" step="1" value="" placeholder="Введите координату 'Y' фигуры">
+<!--    <name="type"><value="circle">-->
+    Тип фигуры: <select name="type"><option value="circle">Круг</option></select>
+    <input type="text" name="radius" step="1" placeholder="Введите радиус круга">
+    <input type="text" name="x" step="1"  placeholder="Введите координату 'X' фигуры">
+    <input type="text" name="y" step="1" placeholder="Введите координату 'Y' фигуры">
     <input type="submit" value="Результат"><hr />
 </form>
 <form action="?square=1" method="post">
-    <input type="number" name="height" step="1" value="" placeholder="Введите высоту прямоугольника">
-    <input type="number" name="width" step="1" value="" placeholder="Введите ширину прямоугольника">
-    <input type="number" name="x" step="1" value="" placeholder="Введите координату 'X' фигуры">
-    <input type="number" name="y" step="1" value="" placeholder="Введите координату 'Y' фигуры">
+    Тип фигуры: <select name="type"><option value="square">Прятоугольник</option></select>
+    <input type="text" name="height" step="1" placeholder="Введите высоту прямоугольника">
+    <input type="text" name="width" step="1" placeholder="Введите ширину прямоугольника">
+    <input type="text" name="x" step="1" placeholder="Введите координату 'X' фигуры">
+    <input type="text" name="y" step="1" placeholder="Введите координату 'Y' фигуры">
     <input type="submit" value="Результат"><hr />
 </form>
 <form action="?treangle=1" method="post">
-    <input type="number" name="height" step="1" value="" placeholder="Введите высоту треугольника">
-    <input type="number" name="width" step="1" value="" placeholder="Введите основание треугольника">
-    <input type="number" name="x" step="1" value="" placeholder="Введите координату 'X' фигуры">
-    <input type="number" name="y" step="1" value="" placeholder="Введите координату 'Y' фигуры">
+    Тип фигуры: <select name="type"><option value="treangle">Треугольник</option></select>
+    <input type="text" name="height" step="1" placeholder="Введите высоту треугольника">
+    <input type="text" name="width" step="1" placeholder="Введите основание треугольника">
+    <input type="text" name="x" step="1" placeholder="Введите координату 'X' фигуры">
+    <input type="text" name="y" step="1" placeholder="Введите координату 'Y' фигуры">
     <input type="submit" value="Результат"><hr />
 </form>
 <?php
